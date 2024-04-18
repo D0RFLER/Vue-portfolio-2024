bbd3205df83df854d3001e2a53168d1888a63086
D0RFLER <fredor01@easv365.dk>
2024-04-19 00:49:39 +0200

    Layout + Kommentarer (Contact)
    Rettelse af layout og kommentarer. Samt er de elementer som ikke virkede blevet fjernet... Og ikke at forglemme dem som ikke havde en funktion

--- Vue Portfolio 2024/src/views/ContactView.vue
+++ Vue Portfolio 2024/src/views/ContactView.vue
@@ -1,58 +1,53 @@
 <template>
- 
-        <div class="body-contact"> <!-- Weird body  -->
+    <div class="body-contact"> <!-- Weird body  -->
 
-            <!-------HEADER  -------->
+<!-------HEADER  --------------------------------------------------------->
 
-                <div class="header">
-                    <div class="logo-header">
-                        <router-link to="/home">
-                            <img src="../assets/LOGO + IKONER/LOGO.svg" alt="">
-                        </router-link>
-                    </div>
+            <div class="header">
+                <div class="logo-header">
+                    <router-link to="/home">
+                        <img src="../assets/LOGO + IKONER/LOGO.svg" alt="">
+                    </router-link>
+                </div>
 
-                    <div class="nav-links">
-            
-                        <!-- <router-link to="/home">Home</router-link> (Skal være logo :> )-->
-                          <!-- <router-link to="/project" class="nav-link">PROJECTS</router-link> -->
+                <div class="nav-links">
                             <router-link to="/gallery" class="nav-link">GALLERY</router-link>
                             <router-link to="/about" class="nav-link">ABOUT</router-link>
                             <router-link to="/contact" class="nav-link">CONTACT</router-link>
-
-                    </div>
-        </div>
-
-            <!-- INDEHOLD ------------------>
-        <div class="contact">
-            <div class="contact-text-img"> <!-- Kompetencer fancy skirft Billede -->
-                <img src="../assets/ILLUSTRATIONER/Contact Skills Text.svg" alt="">
-            
+                </div>
             </div>
 
+<!-- INDEHOLD ------------------------------------------------------------------------------------>
+        
+<div class="contact">
+    <div class="contact-text-img"> <!-- Kompetencer fancy skirft Billede -->
+        <img src="../assets/ILLUSTRATIONER/Contact Skills Text.svg" alt="">           
+    </div>
+
             
-                <div class="contact-text"> <!-- Infomationstekst -->
+    <div class="contact-text"> <!-- Infomationstekst -->
 
             <p>My style is playful, illustrative and intricate. 
                 If it sounds like it will fit into your project, then don’t be scared to reach out! I did love to bring your vision to life. 
                     <br>
                 You can reach me through my contact the listed email - I cannot wait to hear from you </p>
 
-                <!-- Email kontakt -->      
-                <div class="email-info">
+        <!-- Email kontakt -->      
+            <div class="email-info">
 
-                    <h3 class="email-headline"> Email </h3>
-                        <br>
-                    <a href="contact@furd.com" class="email-link">contact@furd.com</a>
-
-                </div>
+                <h3 class="email-headline"> Email </h3>
+                    <br>
+                <a href="contact@furd.com" class="email-link">contact@furd.com</a>
+                    <!-- Gør at den åbner enheder egen mailprogram og mailen er tastet ind - very nice -->
             </div>
+
         </div>
-        <!-- ____ FOOTER ____________________________________________________ -->
+    </div>
+
+<!-- FOOTER ------------------------------------------------------->
 
     <div class="footer">
-        <!-- Left Section -->
         <div class="left">
-            <!-- Logo Icons Container -->
             <div class="logo-icons">
                 <a href="https://www.facebook.com/Freia.Ulla.Rask.Dorfler/" target="_blank">
                     <img src="../assets/icons8-facebook.svg" alt="" class ="pictogram">
@@ -67,13 +62,13 @@
                 </a> <!-- Target blank gør at den åbner i en ny fane -->
             </div>
     
-            <!-- Logo with Text -->
+            
             <div class="tekst-logo"> <!-- Fulde Logo med tekst -->
                 <img src="../assets/LOGO + IKONER/LOGO - with text.svg" alt="" class ="pictogram">
             </div>
         </div>
     
-        <!-- Right Section -->
+        
         <div class="right">
             <div class="colour-codes">
                 <div class="rounded-square-1"></div>
@@ -83,16 +78,19 @@
                 <div class="rounded-square-5"></div>
             </div>
         </div>
+
     </div>
 
-    </div> <!-- Final div -->
-
+</div> <!-- Final div -->
 </template>
+
+<!-- Script setup ---------------------------------------------------------->
 
     <script setup>
   
     </script>
-  
+
+<!-- Styles ---------------------------------------------------------------------->
   <style>
         .body-contact {
             background-color: #f6f6f6;
@@ -114,7 +112,7 @@
             /* Ny størrelse - scale down */
             width: 75%;
             height: 85;
-        }
+            }
 
         .contact-text {
             color: #0b0940; /* Den mørskeste blå - tekst */
@@ -124,9 +122,8 @@
 
             /* placering */
             margin: 50px;
-            padding-top: 50px;
-            
-        }
+            padding-top: 50px;  
+            }
 
         .email-info {
             color: #0b0940; /* Den mørskeste blå - tekst */
@@ -136,163 +133,155 @@
 
             /* placering */
             margin-top: 128px;
-        }
+            }
 
         .email-link {
             color: #0b0940; /* Link farve */
             font-family: "PT Sans Narrow", sans-serif; /* Brødtekst */
             font-weight: 400; /* Tykkelse */
             font-style: normal;
-        }
+            }
 
         .email-headline {
             color: #27368c; 
             font-family: "Bebas Neue", sans-serif;
             font-weight: 400;
             font-style: normal; 
-        }
+            }
 
         .tekst-logo {
             width: 100%;
             height: auto;
+            }
+
+/* ----- FOOTER ---------------------------------------------------------------------- */
+
+    .footer {
+        padding-top: 25px;
+        display: flex;
+        justify-content: space-between; 
+        align-items: center; 
         }
 
-/* ----- FOOTER ----- */
+    .left  {
+        display: flex;
+        align-items: center;
+        }
 
-.footer {
-    padding-top: 25px;
-    display: flex;
-    justify-content: space-between; 
-    align-items: center; 
-}
+    .right {
+        align-items: flex-end;
+        }
 
-.left  {
-    display: flex;
-    align-items: center;
-}
+    .logo-icons {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
 
-.right {
-    align-items: flex-end;
-}
+        margin-left: 15px;
+        margin-right: 5px;
+        }
 
-.logo-icons {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+    .logo-icons img {
+        width: 30px;
+        height: 30px; 
+        }
 
-    margin-left: 15px;
-    margin-right: 5px;
-}
+    .colour-codes {
+        display: flex;
+        justify-content: flex-end;
+        }
 
-.logo-icons img {
-    width: 30px;
-    height: 30px; 
-}
+    .tekst-logo {
+        margin-right: 10px; /* Giver bedre rum */
+        }
 
-.colour-codes {
-    display: flex;
-    justify-content: flex-end;
-}
+    .tekst-logo img {
+        height: 150px; /* Matcher logo højden */
+        width: auto; 
+        }
 
-.tekst-logo {
-    margin-right: 10px; /* Giver bedre rum */
-}
+/* Square Farvekoder ----------------------------------------------- */
 
-.tekst-logo img {
-    height: 150px; /* Matcher logo højden */
-    width: auto; 
-}
-/* Square Farvekoder */
+    .rounded-square {
+        width: 30px;
+        height: 30px;
+        border-radius: 1px;
 
-.rounded-square {
-    width: 30px;
-    height: 30px;
-    border-radius: 1px;
+        border-radius: 10px;
+        margin-right: 5px; /* Mellemrum mellem de forskellige farve-kode-klumber */
+        }
 
-    border-radius: 10px;
-    margin-right: 5px; /* Mellemrum mellem de forskellige farve-kode-klumber */
-}
+    .rounded-square-1 {
+        background-color: #f6f6f6;
+        border: 2px #27368C;
 
-.rounded-square-1 {
-    background-color: #f6f6f6;
-    border: 2px #27368C;
+        width: 30px;
+        height: 30px;
+        border-radius: 1px;
 
-    width: 30px;
-    height: 30px;
-    border-radius: 1px;
+        border-radius: 10px;
+        margin-right: 5px; /* Mellemrum mellem de forskellige farve-kode-klumber */
+        }
 
-    border-radius: 10px;
-    margin-right: 5px; /* Mellemrum mellem de forskellige farve-kode-klumber */
-}
+    .rounded-square-2 {
+        background-color: #658CBF;
 
-.rounded-square-2 {
-    background-color: #658CBF;
+        width: 30px;
+        height: 30px;
+        border-radius: 1px;
 
-    width: 30px;
-    height: 30px;
-    border-radius: 1px;
+        border-radius: 10px;
+        margin-right: 5px; /* Mellemrum mellem de forskellige farve-kode-klumber */
+        }
 
-    border-radius: 10px;
-    margin-right: 5px; /* Mellemrum mellem de forskellige farve-kode-klumber */
-}
+    .rounded-square-3 {
+        background-color: #27368C;
 
-.rounded-square-3 {
-    background-color: #27368C;
+        width: 30px;
+        height: 30px;
+        border-radius: 1px;
 
-    width: 30px;
-    height: 30px;
-    border-radius: 1px;
+        border-radius: 10px;
+        margin-right: 5px; /* Mellemrum mellem de forskellige farve-kode-klumber */
+        }
 
-    border-radius: 10px;
-    margin-right: 5px; /* Mellemrum mellem de forskellige farve-kode-klumber */
-}
+    .rounded-square-4 {
+        background-color: #191659;
 
-.rounded-square-4 {
-    background-color: #191659;
+        width: 30px;
+        height: 30px;
+        border-radius: 1px;
 
-    width: 30px;
-    height: 30px;
-    border-radius: 1px;
+        border-radius: 10px;
+        margin-right: 5px; /* Mellemrum mellem de forskellige farve-kode-klumber */
+        }
 
-    border-radius: 10px;
-    margin-right: 5px; /* Mellemrum mellem de forskellige farve-kode-klumber */
-}
+    .rounded-square-5 {
+        background-color: #0B0940;
 
-.rounded-square-5 {
-    background-color: #0B0940;
+        width: 30px;
+        height: 30px;
+        border-radius: 1px;
 
-    width: 30px;
-    height: 30px;
-    border-radius: 1px;
+        border-radius: 10px;
+        margin-right: 5px; /* Mellemrum mellem de forskellige farve-kode-klumber */
+        }
 
-    border-radius: 10px;
-    margin-right: 5px; /* Mellemrum mellem de forskellige farve-kode-klumber */
-}
 
-/* Tekst + farve kaos */
-.copy-right {
-    color: rgba(0, 0, 0, 0.3);
-    font-size: 12px;
-}
+/* CSS for smaller screens -------------------------------------------------------*/
+    @media only screen and (max-width: 764px) {
+        .contact {
+            flex-direction: column; /* Ændre flex */
+            }
 
-.pictogram {
-    stroke: #27368c;
-}
+        .contact-text {
+            width: 100%;
+            }
 
-/* CSS for smaller screens */
-@media only screen and (max-width: 764px) {
-    .contact {
-        flex-direction: column; /* Change flex direction to column */
+        .contact-text-img {
+            order: -1;
+            align-self: stretch;
+            }
     }
-
-    .contact-text {
-        width: 100%; /* Make both elements full width */
-    }
-
-    .contact-text-img {
-        order: -1; /* Move the image above the text */
-        align-self: stretch; /* Stretch the image to fill the container horizontally */
-    }
-}
 
 </style> 
